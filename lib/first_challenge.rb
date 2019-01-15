c8c80a46c0e5e9ffc6d94fff984d23b9bf203a66
Wendy Hamilton <wdhamilton117@gmail.com>
2019-01-15 20:11:30 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -14,7 +14,7 @@ def first_challenge
   }
 
   #your code here
-contacts.delete["Freddy Mercury"][:favorite_icecream_flavors]["strawberry"]
+contacts.{Freddy Mercury (favorite_icecream_flavors: "strawberry")}
 
   #remember to return your newly altered contacts hash!
   contacts

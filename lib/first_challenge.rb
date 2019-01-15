9338183a9be9787e7de80d435a063950bec2e488
Wendy Hamilton <wdhamilton117@gmail.com>
2019-01-15 20:17:28 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -14,7 +14,7 @@ def first_challenge
   }
 
   #your code here
-contacts.delete(favorite_icecream_flavors: "strawberry")
+contacts.delete("strawberry")
 
   #remember to return your newly altered contacts hash!
   contacts

137e459d1a5a534c73652294a2ad47f5a0512b69
Wendy Hamilton <wdhamilton117@gmail.com>
2019-01-15 20:29:52 +0000

    Done.

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,3 +1,4 @@
+
 def second_challenge
   groceries = {
    dairy: ["milk", "yogurt", "cheese"],
@@ -7,7 +8,7 @@ def second_challenge
   }
 
   #code your solution here!
+  groceries.values.flatten
 
-  
 
 end
